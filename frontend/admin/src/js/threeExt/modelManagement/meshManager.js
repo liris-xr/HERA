d19cbb0adba27e1ae9d5dfa5bed224aa1d4f7490
Amélia <amelia.dimart@gmail.com>
2025-03-13 10:21:10 +0100

    loading meshes for the first time working

--- frontend/admin/src/js/threeExt/modelManagement/meshManager.js
+++ frontend/admin/src/js/threeExt/modelManagement/meshManager.js
@@ -15,24 +15,25 @@ export class MeshManager {
     });
 
     addSubMesh(scene,mesh,meshData,onAdd) {
-        
-        mesh.position.x = meshData.position.x 
-        mesh.position.y = meshData.position.y 
-        mesh.position.z = meshData.position.z 
-        
-        mesh.rotation.x = meshData.rotation._x
-        mesh.rotation.y = meshData.rotation._y
-        mesh.rotation.z = meshData.rotation._z
-        mesh.scale.x = meshData.scale.x
-        mesh.scale.y = meshData.scale.y
-        mesh.scale.z = meshData.scale.z 
-        
-        mesh.material.opacity = meshData.opacity
-        mesh.material.transparent = meshData.opacity != 1
-        mesh.material.emissiveColor = meshData.emissiveColor
-        mesh.material.emissiveIntensity = meshData.emissiveIntensity
-        mesh.material.roughness = meshData.roughness
-        mesh.material.metalness = meshData.metalness
+        if(meshData) {
+            mesh.position.x = meshData.position.x 
+            mesh.position.y = meshData.position.y 
+            mesh.position.z = meshData.position.z 
+            
+            mesh.rotation.x = meshData.rotation._x
+            mesh.rotation.y = meshData.rotation._y
+            mesh.rotation.z = meshData.rotation._z
+            mesh.scale.x = meshData.scale.x
+            mesh.scale.y = meshData.scale.y
+            mesh.scale.z = meshData.scale.z 
+            
+            mesh.material.opacity = meshData.opacity
+            mesh.material.transparent = meshData.opacity != 1
+            mesh.material.emissiveColor = meshData.emissiveColor
+            mesh.material.emissiveIntensity = meshData.emissiveIntensity
+            mesh.material.roughness = meshData.roughness
+            mesh.material.metalness = meshData.metalness
+        }
         
         scene.add( mesh );
         this.#meshes.push(mesh)

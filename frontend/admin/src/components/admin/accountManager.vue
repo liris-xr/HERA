b17e941b4cdd61d08cb1dbdeab50afaf22185d34
JonathanOll <jonathan.ol01100@gmail.com>
2025-04-22 09:05:46 +0200

    division en components

---
+++ frontend/admin/src/components/admin/accountManager.vue
@@ -0,0 +1,210 @@
+<script setup>
+import {ENDPOINT} from "@/js/endpoints.js";
+import {onMounted, ref} from "vue";
+import ButtonView from "@/components/button/buttonView.vue";
+
+
+const props = defineProps({
+  token: {type: String, required: true},
+})
+
+
+const users = ref([])
+const editingUser = ref(null)
+const deletingUser = ref(null)
+const creatingUser = ref(null)
+
+
+async function confirmUserEdit() {
+  const res = await fetch(`${ENDPOINT}admin/users/${editingUser.value.id}`,{
+    method: "PUT",
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${props.token}`,
+    },
+    body: JSON.stringify(editingUser.value),
+  })
+
+  if(res.ok) {
+    const data = await res.json()
+
+    const index = users.value.findIndex(user => user.id === data.id)
+    if(index !== -1)
+      users.value[index] = { ...data }
+  }
+
+  editingUser.value = null
+}
+
+async function confirmUserDelete() {
+  const res = await fetch(`${ENDPOINT}admin/users/${deletingUser.value.id}`,{
+    method: "DELETE",
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${props.token}`,
+    },
+  })
+
+  if(res.ok) {
+    const index = users.value.findIndex(user => user.id === deletingUser.value.id)
+    if(index !== -1)
+      users.value.splice(index, index+1)
+  }
+  deletingUser.value = null
+}
+
+async function confirmUserCreate() {
+  const res = await fetch(`${ENDPOINT}admin/users`,{
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${props.token}`,
+    },
+    body: JSON.stringify(creatingUser.value),
+  })
+
+  if(res.ok) {
+    const newUser = await res.json()
+    users.value.push(newUser)
+  }
+
+  creatingUser.value = null
+}
+
+async function fetchUsers() {
+  console.log("fetching users")
+  try {
+    const res = await fetch(`${ENDPOINT}admin/users/`,
+        {
+          headers: {
+            'Authorization': `Bearer ${props.token}`,
+          }
+        })
+
+    if(res.ok) {
+      return await res.json()
+    }
+    throw new Error("ko")
+  } catch(error) {
+    console.log(error)
+    //TODO
+  }
+}
+
+onMounted(async () => {
+  users.value = await fetchUsers()
+})
+
+</script>
+
+<template>
+  <section>
+    <h2>{{$t("admin.sections.accounts.title")}}</h2>
+
+    <table>
+      <thead>
+      <tr>
+        <th>{{$t("admin.sections.accounts.username")}}</th>
+        <th>{{$t("admin.sections.accounts.email")}}</th>
+        <th>{{$t("admin.sections.accounts.actions")}}</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="user in users">
+        <td>{{user.username}}</td>
+        <td>{{user.email}}</td>
+        <td>
+          <div class="inline-flex">
+            <button-view icon="/icons/edit.svg" @click="editingUser = {...user}"></button-view>
+            <button-view icon="/icons/delete.svg" theme="danger" @click="deletingUser = user"></button-view>
+          </div>
+        </td>
+      </tr>
+
+      <tr>
+        <td colspan="3">
+          <div class="inline-flex">
+            <button-view icon="/icons/add.svg" @click="creatingUser = {}"></button-view>
+          </div>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+  </section>
+
+  <!-- Interfaces modales -->
+
+  <!-- user -->
+
+  <div class="modal" v-if="editingUser">
+    <div>
+      <h2>Modifier l'utilisateur</h2>
+      <div>
+        <label for="username">{{$t("admin.sections.accounts.username")}}</label>
+        <input v-model="editingUser.username" id="username" name="username">
+      </div>
+      <div>
+        <label for="email">{{$t("admin.sections.accounts.email")}}</label>
+        <input v-model="editingUser.email" id="email" name="email">
+      </div>
+      <div>
+        <label for="admin">{{$t("admin.sections.accounts.admin")}}</label>
+        <input v-model="editingUser.admin" type="checkbox" id="admin" name="admin">
+      </div>
+      <div>
+        <button @click="confirmUserEdit">Confirmer</button>
+      </div>
+      <div>
+        <button @click="editingUser = null">Annuler</button>
+      </div>
+    </div>
+  </div>
+
+  <div class="modal" v-if="deletingUser">
+    <div>
+      <h2>Supprimer l'utilisateur</h2>
+      <div>
+        <p>Vous-vous vraiment supprimer {{deletingUser.username}} ?</p>
+        <p class="danger">⚠Cette action est irréversible⚠</p>
+      </div>
+      <div>
+        <button @click="confirmUserDelete">Confirmer</button>
+      </div>
+      <div>
+        <button @click="deletingUser = null">Annuler</button>
+      </div>
+    </div>
+  </div>
+
+  <div class="modal" v-if="creatingUser">
+    <div>
+      <h2>Créer un utilisateur</h2>
+      <div>
+        <label for="username">{{$t("admin.sections.accounts.username")}}</label>
+        <input v-model="creatingUser.username" id="username" name="username" placeholder="Jean01000">
+      </div>
+      <div>
+        <label for="email">{{$t("admin.sections.accounts.email")}}</label>
+        <input v-model="creatingUser.email" id="email" name="email" placeholder="Jean">
+      </div>
+      <div>
+        <label for="password">{{$t("admin.sections.accounts.password")}}</label>
+        <input v-model="creatingUser.password" type="password" id="password" name="password" placeholder="********">
+      </div>
+      <div>
+        <label for="admin">{{$t("admin.sections.accounts.admin")}}</label>
+        <input v-model="creatingUser.admin" type="checkbox" id="admin" name="admin">
+      </div>
+      <div>
+        <button @click="confirmUserCreate">Confirmer</button>
+      </div>
+      <div>
+        <button @click="creatingUser = null">Annuler</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- frontend/admin/src/views/AdminView.vue
+++ frontend/admin/src/views/AdminView.vue
@@ -10,6 +10,7 @@ import FilledButtonView from "@/components/button/filledButtonView.vue"
 import TextInputModal from "@/components/modal/textInputModal.vue"
 import RedirectMessage from "@/components/notification/redirect-message.vue"
 import {useI18n} from "vue-i18n"
+import AccountManager from "@/components/admin/accountManager.vue";
 
 
 const { isAuthenticated, token ,userData} = useAuthStore()
@@ -17,10 +18,7 @@ const router = useRouter()
 const {t} = useI18n()
 
 
-const users = ref([])
-const editingUser = ref(null)
-const deletingUser = ref(null)
-const creatingUser = ref(null)
+
 
 if (!isAuthenticated.value) {
   router.push({ name: "login" })
@@ -30,83 +28,6 @@ if(!userData.value.admin) {
   router.push({ name: "home" })
 }
 
-async function confirmUserEdit() {
-  const res = await fetch(`${ENDPOINT}admin/users/${editingUser.value.id}`,{
-    method: "PUT",
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token.value}`,
-    },
-    body: JSON.stringify(editingUser.value),
-  })
-
-  if(res.ok) {
-    const data = await res.json()
-
-    const index = users.value.findIndex(user => user.id === data.id)
-    if(index !== -1)
-      users.value[index] = { ...data }
-  }
-
-  editingUser.value = null
-}
-
-async function confirmUserDelete() {
-  const res = await fetch(`${ENDPOINT}admin/users/${deletingUser.value.id}`,{
-    method: "DELETE",
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token.value}`,
-    },
-  })
-
-  if(res.ok) {
-    const index = users.value.findIndex(user => user.id === deletingUser.value.id)
-    if(index !== -1)
-      users.value.splice(index, index+1)
-  }
-  deletingUser.value = null
-}
-
-async function confirmUserCreate() {
-  const res = await fetch(`${ENDPOINT}admin/users`,{
-    method: "POST",
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token.value}`,
-    },
-    body: JSON.stringify(creatingUser.value),
-  })
-
-  if(res.ok) {
-    const newUser = await res.json()
-    users.value.push(newUser)
-  }
-
-  creatingUser.value = null
-}
-
-async function fetchUsers() {
-  try {
-    const res = await fetch(`${ENDPOINT}admin/users/`,
-        {
-          headers: {
-            'Authorization': `Bearer ${token.value}`,
-          }
-        })
-
-    if(res.ok) {
-      return await res.json()
-    }
-    throw new Error("ko")
-  } catch(error) {
-    //TODO
-  }
-}
-
-onMounted(async () => {
-  users.value = await fetchUsers()
-})
 
 
 </script>
@@ -116,39 +37,7 @@ onMounted(async () => {
 
     <h1>{{$t("admin.title")}}</h1>
 
-    <section>
-      <h2>{{$t("admin.sections.accounts.title")}}</h2>
-
-      <table>
-        <thead>
-          <tr>
-            <th>{{$t("admin.sections.accounts.username")}}</th>
-            <th>{{$t("admin.sections.accounts.email")}}</th>
-            <th>{{$t("admin.sections.accounts.actions")}}</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="user in users">
-            <td>{{user.username}}</td>
-            <td>{{user.email}}</td>
-            <td>
-              <div class="inline-flex">
-                <button-view icon="/icons/edit.svg" @click="editingUser = {...user}"></button-view>
-                <button-view icon="/icons/delete.svg" theme="danger" @click="deletingUser = user"></button-view>
-              </div>
-            </td>
-          </tr>
-
-          <tr>
-            <td colspan="3">
-              <div class="inline-flex">
-                <button-view icon="/icons/add.svg" @click="creatingUser = {}"></button-view>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </section>
+    <account-manager :token="token"></account-manager>
 
     <section>
       <h2>{{$t("admin.sections.projects.title")}}</h2>
@@ -173,82 +62,12 @@ onMounted(async () => {
 
   </main>
 
-  <!-- Interfaces modales -->
 
-  <!-- user -->
-
-  <div class="modal" v-if="editingUser">
-    <div>
-      <h2>Modifier l'utilisateur</h2>
-      <div>
-        <label for="username">{{$t("admin.sections.accounts.username")}}</label>
-        <input v-model="editingUser.username" id="username" name="username">
-      </div>
-      <div>
-        <label for="email">{{$t("admin.sections.accounts.email")}}</label>
-        <input v-model="editingUser.email" id="email" name="email">
-      </div>
-      <div>
-        <label for="admin">{{$t("admin.sections.accounts.admin")}}</label>
-        <input v-model="editingUser.admin" type="checkbox" id="admin" name="admin">
-      </div>
-      <div>
-        <button @click="confirmUserEdit">Confirmer</button>
-      </div>
-      <div>
-        <button @click="editingUser = null">Annuler</button>
-      </div>
-    </div>
-  </div>
-
-  <div class="modal" v-if="deletingUser">
-    <div>
-      <h2>Supprimer l'utilisateur</h2>
-      <div>
-        <p>Vous-vous vraiment supprimer {{deletingUser.username}} ?</p>
-        <p class="danger">⚠Cette action est irréversible⚠</p>
-      </div>
-      <div>
-        <button @click="confirmUserDelete">Confirmer</button>
-      </div>
-      <div>
-        <button @click="deletingUser = null">Annuler</button>
-      </div>
-    </div>
-  </div>
-
-  <div class="modal" v-if="creatingUser">
-    <div>
-      <h2>Créer un utilisateur</h2>
-      <div>
-        <label for="username">{{$t("admin.sections.accounts.username")}}</label>
-        <input v-model="creatingUser.username" id="username" name="username" placeholder="Jean01000">
-      </div>
-      <div>
-        <label for="email">{{$t("admin.sections.accounts.email")}}</label>
-        <input v-model="creatingUser.email" id="email" name="email" placeholder="Jean">
-      </div>
-      <div>
-        <label for="password">{{$t("admin.sections.accounts.password")}}</label>
-        <input v-model="creatingUser.password" type="password" id="password" name="password" placeholder="********">
-      </div>
-      <div>
-        <label for="admin">{{$t("admin.sections.accounts.admin")}}</label>
-        <input v-model="creatingUser.admin" type="checkbox" id="admin" name="admin">
-      </div>
-      <div>
-        <button @click="confirmUserCreate">Confirmer</button>
-      </div>
-      <div>
-        <button @click="creatingUser = null">Annuler</button>
-      </div>
-    </div>
-  </div>
 
 </template>
 
 
-<style scoped>
+<style >
 
 table {
   width: 80%;

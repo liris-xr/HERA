87541729851bc6e73156a601eabc548cf73b408f
Amélia <amelia.dimart@gmail.com>
2025-05-07 19:00:28 +0200

    fixed indirect lighting

--- frontend/admin/src/js/threeExt/lighting/lightSet.js
+++ frontend/admin/src/js/threeExt/lighting/lightSet.js
@@ -21,8 +21,8 @@ export class LightSet extends classes(THREE.Group,SceneElementInterface) {
         this.add(ambientLight);
         this.add(this.#directionalLight);
 
-        const lightProbeVolume = new LightProbeVolume(new THREE.Vector3(0,1,0),8,2,2,2,scene);
-        lightProbeVolume.bake(1,16,32,16);
+        const lightProbeVolume = new LightProbeVolume(new THREE.Vector3(0,1,0),20,2,2,2,scene);
+        lightProbeVolume.bake(1,16,64,16);
     }
     
     pushToScene(scene){

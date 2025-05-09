2b903aa09ec04922ee6faf420bfcc3674d8e8fce
Sorbexos <amelia.dimart@gmail.com>
2025-05-09 15:39:39 +0200

    forgor

---
+++ gkit2light/src/gKit/lightSources.hpp
@@ -0,0 +1,33 @@
+#ifndef _LIGHT_SOURCES_H
+#define _LIGHT_SOURCES_H
+
+#include "mesh.h"
+#include "gltf.h"
+#include <random>
+#include <vector>
+
+// Tableau des triangles émissifs de la scène
+// + Tableau de pondération en fonction de la taille des triangles
+class LightSources {  
+    private:
+    
+        std::vector<unsigned int> triangleIds;
+        std::vector<float> weights;
+        std::vector<Color> triangleColor;
+        float area;
+
+        std::discrete_distribution<unsigned int> dd;
+
+        
+        
+    public:
+        LightSources(const Mesh & mesh,const std::vector<GLTFMaterial> & materials);
+        ~LightSources();
+        
+        unsigned int getRandomWeightedLightSourceId();
+        unsigned int getTriangleId(unsigned int lightSourceId);
+        Color getTriangleColor(unsigned int lightSourceId);
+
+};
+
+#endif

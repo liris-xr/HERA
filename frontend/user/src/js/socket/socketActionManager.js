574655f3593b27af28089dacc0d4fee3413e6c06
Jonathan O <jonathan.ol01100@gmail.com>
2025-06-02 09:49:00 +0200

    fix

--- frontend/user/src/js/socket/socketActionManager.js
+++ frontend/user/src/js/socket/socketActionManager.js
@@ -62,26 +62,6 @@ export class SocketActionManager {
         if(!asset) return
 
         asset.playAnimation(data.value)
-        return
-
-        if(asset.playingAction) {
-            asset.playingAction.stop()
-            asset.playingAction = null
-            asset.activeAnimation = null
-        }
-
-        if(data.value) {
-            const anim = THREE.AnimationClip.findByName(asset.object.animations, data.value)
-
-            if(!anim) return
-
-            const action = asset.animationMixer.clipAction(anim)
-            action.play()
-            asset.activeAnimation = data.value
-
-            asset.playingAction = action
-
-        }
     }
 
     toggleLabel(data) {

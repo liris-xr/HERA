df872a467a36b022ab6f1fe56aee460f33d4d246
Pierre Jouffroy <83352112+rageofpseudo@users.noreply.github.com>
2026-02-20 11:04:17 +0100

    temp commit to checkout

--- backend/api/src/orm/index.js
+++ backend/api/src/orm/index.js
@@ -2,10 +2,11 @@ import ArUser from './models/arUser.js'
 import ArProject from "./models/arProject.js";
 import ArScene from "./models/arScene.js";
 
-import {sequelize, sequelizeRecords} from './database.js'
+import {sequelize} from './database.js'
 import ArAsset from "./models/arAsset.js";
 import ArMesh from "./models/arMesh.js";
 import ArLabel from "./models/arLabel.js";
+import ArSound from "./models/arSound.js";
 
 ArUser.hasMany(ArProject, { as: 'projects', foreignKey: 'userId', onDelete: 'CASCADE' });
 
@@ -16,15 +17,16 @@ ArScene.belongsTo(ArProject, { as: 'project', foreignKey: 'projectId' });
 ArScene.hasMany(ArAsset, { as: 'assets', foreignKey: 'sceneId', onDelete: 'CASCADE' });
 ArScene.hasMany(ArLabel, {as: 'labels', foreignKey: 'sceneId', onDelete: 'CASCADE' });
 ArScene.hasMany(ArMesh, {as: 'meshes', foreignKey: 'sceneId', onDelete: 'CASCADE' });
+ArScene.hasMany(ArSound, {as: 'sounds', foreignKey: 'sceneId', onDelete: 'CASCADE' });
 
 ArAsset.belongsTo(ArScene, { as: 'scene', foreignKey: 'sceneId' });
 ArMesh.belongsTo(ArScene, { as: 'scene', foreignKey: 'sceneId' });
 ArLabel.belongsTo(ArScene, { as: 'scene', foreignKey: 'sceneId' });
-
+ArSound.belongsTo(ArSound, { as: 'scene', foreignKey: 'sceneId' });
 
 export async function initializeDatabase (options) {
-    await sequelizeRecords.sync(options);
-    await sequelize.sync(options);
+    return await sequelize.sync(options);
 }
 
-export {ArUser, ArProject, ArScene, ArAsset, ArLabel, ArMesh}
+export {ArUser, ArProject, ArScene, ArAsset, ArLabel, ArMesh, ArSound}
+

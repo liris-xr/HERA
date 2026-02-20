a594cc216b49da69d5e2007d6737c946eb652521
Pierre Jouffroy <83352112+rageofpseudo@users.noreply.github.com>
2026-02-20 10:47:04 +0100

    feature: debut ajout son

---
+++ frontend/admin/src/js/soundManagement/sound.js
@@ -0,0 +1,95 @@
+import {classes} from "@/js/utils/extender.js";
+import {SelectableInterface} from "@/js/threeExt/interfaces/selectableInterface.js";
+import {LoadableInterface} from "@/js/threeExt/interfaces/loadableInterface.js";
+import {computed, ref} from "vue";
+
+export class Sound extends classes(SelectableInterface, LoadableInterface){
+    id;
+    url;
+    copiedUrl;
+    uploadData;
+    name;
+
+    playOnStartup;
+    isLoopingEnabled;
+
+    volumeLevel
+    isPlaying
+
+    #hasError;
+    #isLoading;
+
+
+    constructor(soundData) {
+        super();
+        this.id = soundData.id;
+        this.name = soundData.name;
+        this.url = soundData.url;
+        this.uploadData = soundData.uploadData || null;
+
+        if(soundData.playOnStartup){
+            this.playOnStartup = ref(soundData.playOnStartup);
+        }
+        else{
+            this.playOnStartup = ref(false);
+        }
+
+        if(soundData.isLoopingEnabled){
+            this.isLoopingEnabled = ref(soundData.isLoopingEnabled);
+        }
+        else{
+            this.isLoopingEnabled = ref(false);
+        }
+
+        if(soundData?.copiedUrl)
+            this.copiedUrl = soundData.copiedUrl;
+
+        if(soundData?.volumeLevel){
+            this.volumeLevel = soundData.volumeLevel;
+        }
+        else{
+            this.volumeLevel = "0.5";
+        }
+
+        this.#hasError = ref(false);
+        this.#isLoading = ref(false);
+        this.isPlaying = ref(false);
+    }
+
+    hasError = computed(()=>{
+        return this.#hasError.value;
+    })
+
+    isLoading = computed(()=>{
+        return this.#isLoading.value;
+    })
+
+    isSelected = computed(()=>false);
+
+
+    switchPlayOnStartupStatus(status){
+        this.playOnStartup.value = status;
+    }
+
+    switchLoopingEnabledStatus(status){
+        this.isLoopingEnabled.value = status;
+    }
+
+    setUploadedAtUrl(url){
+        this.uploadData = null;
+        this.url = url;
+    }
+
+    play(){
+        this.isPlaying.value = true;
+    }
+
+    stop(){
+        this.isPlaying.value = false;
+    }
+
+
+    setVolume(level){
+        this.volumeLevel = level;
+    }
+}
